d35e3a369e89d910c8e723cdbed8734ced3284e7
bdrsotelo <bdrsotelo.dev@gmail.com>
2022-08-28 00:38:15 -0600

    Adds problems section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+showLoader()
 //setup navbar
+
 
 let navItems = Array.from(document.getElementsByClassName("nav_item"))
 let sections = Array.from(document.getElementsByClassName("section"))
@@ -6,91 +8,69 @@ let sections = Array.from(document.getElementsByClassName("section"))
 //getcookkies
 let secitonsPosition = getCookie('secitonsPosition') ? getCookie('secitonsPosition') : {git: 0}
 let currentSection = getCookie('currentSection') ? getCookie('currentSection') : 'git'
+console.log(secitonsPosition)
 
-
-
-function scrollToElement(id){
-    console.log(id)
-    console.log($(`#${id}`).offset())
-    setTimeout(() => {        
-        $(window).scrollTop($(`#${id}`).offset().top -80)
-    }, 50);
+function scrollToElement(id){ setTimeout(() => {
+        $("html, body").animate({ scrollTop: $(`#${id}`).offset().top -80 }, 500)
+    },100)
 }
+console.log('x0')
 
 $(`.nav_item-${currentSection}`).addClass("nav_item--selected")
-$(`.section-${currentSection}`).show(250 ,()=> {
-    $(window).scrollTop(secitonsPosition[currentSection])
 
-    window.addEventListener('scroll', e => {
-        secitonsPosition[currentSection]=window.scrollY
-        setCookie('secitonsPosition', JSON.stringify(secitonsPosition), 1)
-    })
+if($(`.section-${currentSection}`).length < 1 ){ currentSection = $(navItems[0]).attr('section') }
 
-    $(`.nav .nav_item`).each((i, navItem) => {
-        let section = $(navItem).attr('section')
+console.log(currentSection)
+$(`.section-${currentSection}`).fadeIn(500, ()=> {
+    setTimeout(() => {
 
-        // let navItemHtml = `<div class="nav_item_flex-container">`
+        $("html, body").animate({ scrollTop: secitonsPosition[currentSection] }, 500)
 
-        // // $(navItem).append("<def1>test</def1>")
-
-        // $(`.section-${section} def1`).each((i, def1) => {
-
-
-
-
-        //     let def1_id = ""+section+$(def1).text().replaceAll(" ", "_")
-        //     $(def1).prop('id', def1_id)
-        //     navItemHtml += `<div class="nav_item_flex-container-item" onclick="scrollToElement('${def1_id}')"> ${$(def1).text()}</div>`
-
-
-        //     $(`#${def1_id} def2`).each((i, def2) => {
-        //         let def2_id = ""+section+$(def2).text().replaceAll(" ", "_")
-        //         $(def2).prop('id', def2_id)
-        //         navItemHtml += `<div class="nav_item_flex-container-item--sec" onclick="scrollToElement('${def2_id}')"> ${$(def2).text()}</div>`
-        //     })
-
-
-        // })
-
-        let navItemHtml = `<div class="nav_item_flex-container">`
-
-        $(`.section-${section} .block`).each((i, block ) => {
-
-            $(block).find(`def1`).each((i, def1) => {
-                let def1_id = ""+section+$(def1).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star")
-                $(def1).prop('id', def1_id)
-                navItemHtml += `<div class="nav_item_flex-container-item" onclick="scrollToElement('${def1_id}')"> ${$(def1).text()}</div>`
-            })
-
-            $(block).find(`def2`).each((i, def2) => {
-                let def2_id = ""+section+$(def2).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star")
-                $(def2).prop('id', def2_id)
-                navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--sec" onclick="scrollToElement('${def2_id}')"> ${$(def2).text()}</div>`
-            })
-
-            $(block).find(`def3`).each((i, def3) => {
-                let def3_id = ""+section+$(def3).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star")
-                $(def3).prop('id', def3_id)
-                navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--third" onclick="scrollToElement('${def3_id}')"> ${$(def3).text()}</div>`
-            })
-
+        window.addEventListener('scroll', e => {
+            secitonsPosition[currentSection]=window.scrollY
+            setCookie('secitonsPosition', JSON.stringify(secitonsPosition), 1)
         })
-        navItemHtml += `</div>`
 
-        $(navItem).append(navItemHtml)
+        console.log('x1')
 
-    })
+        setTimeout(() => {
+            $(`.nav .nav_item`).each((i, navItem) => {
+                let section = $(navItem).attr('section')
 
-    // $(`def1`).each((i, e) => {
-    //     console.log($(e).text())
-    //     $(e).prop('id', "def1_"+$(e).text())
-    // })
+                //Initialize submenu flex container
+                let navItemHtml = `<div class="nav_item_flex-container">`
+                $(`.section-${section} .block`).each((i, block ) => {
 
-    // $(`def2`).each((i, e) => {
-    //     console.log($(e).text())
-    //     $(e).prop('id', "def2_"+$(e).text())
-    // })
-    
+                    //set def1 lements ids, create submenu items
+                    $(block).find(`def1`).each((i, def1) => {
+                        let def1_id = ""+section+$(def1).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star")
+                        $(def1).prop('id', def1_id)
+                        navItemHtml += `<div class="nav_item_flex-container-item" onclick="scrollToElement('${def1_id}')"> ${$(def1).text()}</div>`
+                    })
+
+                    //set def2 lements ids, create submenu items
+                    $(block).find(`def2`).each((i, def2) => {
+                        let def2_id = ""+section+$(def2).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star")
+                        $(def2).prop('id', def2_id)
+                        navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--sec" onclick="scrollToElement('${def2_id}')"> ${$(def2).text()}</div>`
+                    })
+
+                    //set def3 lements ids, create submenu items
+                    $(block).find(`def3`).each((i, def3) => {
+                        let def3_id = ""+section+$(def3).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star")
+                        $(def3).prop('id', def3_id)
+                        navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--third" onclick="scrollToElement('${def3_id}')"> ${$(def3).text()}</div>`
+                    })
+                })
+                //closing element of the submenu container
+
+                navItemHtml += `</div>`
+                //append submenu container with items to the navItem
+                $(navItem).append(navItemHtml)
+                hideLoader(500)
+            })
+        },100)
+    },100)
 })
 
 navItems.forEach(e => {
@@ -150,3 +130,8 @@ function getCookie(key) {
 
 // $(`section-${currentSection} def1`)
 // $(`section-${currentSection} def1`)
+
+
+function showLoader(){ $(".loader").show() }
+function hideLoader(time=0){ setTimeout(()=>{ $(".loader").fadeOut(500)},time)  }
+

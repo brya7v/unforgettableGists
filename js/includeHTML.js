0cd4d38413102126aea82ec7f113dd600351054b
brya7v <brya7v@gmail.com>
2022-11-08 17:49:04 -0600

    Fix sync doc loading and problems structure

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,3 +1,5 @@
+var promises = []
+
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
     /*loop through a collection of all HTML elements:*/
@@ -8,14 +10,19 @@ function includeHTML() {
         file = elmnt.getAttribute("w3-include-html");
         if (file) {
         /*make an HTTP request using the attribute value as the file name:*/
+        
+        xhttp = new XMLHttpRequest();        
+        var outsideRes, outsideRej
+        let reqPromise = new Promise((res, rej) => {
+            outsideRes = res
+            outsideRej = rej
+        })
 
-        xhttp = new XMLHttpRequest();
-        console.log(xhttp)
-
+        promises.push(reqPromise)
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            if (this.status == 200) {elmnt.innerHTML = this.responseText; outsideRes('resolved')}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found."; outsideRej('reject')}
             /*remove the attribute, and call this function once more:*/
             elmnt.removeAttribute("w3-include-html");
             includeHTML();
--- js/main.js
+++ js/main.js
@@ -1,121 +1,126 @@
+includeHTML()
+
 showLoader()
 //setup navbar
+Promise.all([...promises]).then(r=> {
+    console.log('all docs loaded')
 
 
-let navItems = Array.from(document.getElementsByClassName("nav_item"))
-let sections = Array.from(document.getElementsByClassName("section"))
+    let navItems = Array.from(document.getElementsByClassName("nav_item"))
+    let sections = Array.from(document.getElementsByClassName("section"))
 
-//getcookkies
-let secitonsPosition = getCookie('secitonsPosition') ? getCookie('secitonsPosition') : {git: 0}
-let currentSection = getCookie('currentSection') ? getCookie('currentSection') : 'git'
+    //getcookkies
+    let secitonsPosition = getCookie('secitonsPosition') ? getCookie('secitonsPosition') : {git: 0}
+    let currentSection = getCookie('currentSection') ? getCookie('currentSection') : 'git'
 
-document.title = "Unforgettable Gists - " + currentSection
+    document.title = "Unforgettable Gists - " + currentSection
+
+    $(`.nav_item-${currentSection}`).addClass("nav_item--selected")
+
+    if($(`.section-${currentSection}`).length < 1 ){ currentSection = $(navItems[0]).attr('section') }
+
+    $(`.section-${currentSection}`).fadeIn(500, ()=> {
+        setTimeout(() => {
+
+            $("html, body").animate({ scrollTop: secitonsPosition[currentSection] }, 500)
+
+            window.addEventListener('scroll', e => {
+                secitonsPosition[currentSection]=window.scrollY
+                setCookie('secitonsPosition', JSON.stringify(secitonsPosition), 1)
+            })
+
+            setTimeout(() => {
+                $(`.nav .nav_item`).each((i, navItem) => {
+                    let section = $(navItem).attr('section')
+
+                    if(section == "about") return 
+
+                    //Initialize submenu flex container
+                    let navItemHtml = `<div class="nav_item_flex-container">`
+                    $(`.section-${section} .block`).each((i, block ) => {
+
+                        //set def1 lements ids, create submenu items
+                        $(block).find(`def1`).each((i, def1) => {
+                            let def1_id = ""+section+$(def1).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star").replaceAll(".", "dot").replaceAll("(", "_").replaceAll(")", "_").replaceAll("/", "_")
+                            $(def1).prop('id', def1_id)
+                            navItemHtml += `<div class="nav_item_flex-container-item" onclick="scrollToElement('${def1_id}')"> ${$(def1).text()}</div>`
+                        })
+
+                        //set def2 lements ids, create submenu items
+                        $(block).find(`def2`).each((i, def2) => {
+                            let def2_id = ""+section+$(def2).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star").replaceAll(".", "dot").replaceAll("(", "_").replaceAll(")", "_").replaceAll("/", "_")
+                            $(def2).prop('id', def2_id)
+                            navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--sec" onclick="scrollToElement('${def2_id}')"> ${$(def2).text()}</div>`
+
+                            //set def3 lements ids, create submenu items
+                            $(def2).parent().find(`def3`).each((i, def3) => {
+                                let def3_id = ""+section+$(def3).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star").replaceAll(".", "dot").replaceAll("(", "_").replaceAll(")", "_").replaceAll("/", "_")
+                                $(def3).prop('id', def3_id)
+                                navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--third" onclick="scrollToElement('${def3_id}')"> ${$(def3).text()}</div>`
+                            })
+                        })
+
+
+                    })
+                    //closing element of the submenu container
+
+                    navItemHtml += `</div>`
+                    //append submenu container with items to the navItem
+                    $(navItem).append(navItemHtml)
+                    hideLoader(250)
+                })
+            },250)
+        },250)
+    })
+
+    navItems.forEach(e => {
+        e.addEventListener("click", () => {
+
+            //get current window scroll
+            let currentScroll = window.pageYOffset
+
+            //store current scroll
+            secitonsPosition[currentSection] = currentScroll
+
+            //scroll top if new selection is the same as the current one
+            if(currentSection == e.getAttribute("section")){
+                // secitonsPosition[currentSection] = 0
+                // $("html, body").animate({ scrollTop: "0px" })
+            }
+
+            else {
+
+                $(`.nav_item-${currentSection}`).removeClass("nav_item--selected")
+                
+                //set new current selection
+                currentSection = e.getAttribute("section")
+                $(`.nav_item-${currentSection}`).addClass("nav_item--selected")
+
+                //set position when first time section selection
+                if(secitonsPosition[currentSection] == null) secitonsPosition[currentSection] = 0
+
+                //hide unselected sections
+                sections.forEach(s => { if(!s.classList.contains(`section-${currentSection}`)) $(s).hide() })
+
+                //show selected section
+                $(`.section-${currentSection}`).fadeIn(100)
+
+                //scrol to previously store scroll section after showing current section
+                $(window).scrollTop(secitonsPosition[currentSection])
+                document.title = "Unforgettable Gists - " + currentSection
+            }
+
+            setCookie('secitonsPosition', JSON.stringify(secitonsPosition), 1)
+            setCookie('currentSection', JSON.stringify(currentSection), 1)
+        })
+    })
+
+})
 
 function scrollToElement(id){ setTimeout(() => {
-        $("html, body").animate({ scrollTop: $(`#${id}`).offset().top -80 }, 500)
-    },100)
+    $("html, body").animate({ scrollTop: $(`#${id}`).offset().top -80 }, 500)
+},100)
 }
-
-$(`.nav_item-${currentSection}`).addClass("nav_item--selected")
-
-if($(`.section-${currentSection}`).length < 1 ){ currentSection = $(navItems[0]).attr('section') }
-
-$(`.section-${currentSection}`).fadeIn(500, ()=> {
-    setTimeout(() => {
-
-        $("html, body").animate({ scrollTop: secitonsPosition[currentSection] }, 500)
-
-        window.addEventListener('scroll', e => {
-            secitonsPosition[currentSection]=window.scrollY
-            setCookie('secitonsPosition', JSON.stringify(secitonsPosition), 1)
-        })
-
-        setTimeout(() => {
-            $(`.nav .nav_item`).each((i, navItem) => {
-                let section = $(navItem).attr('section')
-
-                if(section == "about") return 
-
-                //Initialize submenu flex container
-                let navItemHtml = `<div class="nav_item_flex-container">`
-                $(`.section-${section} .block`).each((i, block ) => {
-
-                    //set def1 lements ids, create submenu items
-                    $(block).find(`def1`).each((i, def1) => {
-                        let def1_id = ""+section+$(def1).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star").replaceAll(".", "dot").replaceAll("(", "_").replaceAll(")", "_").replaceAll("/", "_")
-                        $(def1).prop('id', def1_id)
-                        navItemHtml += `<div class="nav_item_flex-container-item" onclick="scrollToElement('${def1_id}')"> ${$(def1).text()}</div>`
-                    })
-
-                    //set def2 lements ids, create submenu items
-                    $(block).find(`def2`).each((i, def2) => {
-                        let def2_id = ""+section+$(def2).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star").replaceAll(".", "dot").replaceAll("(", "_").replaceAll(")", "_").replaceAll("/", "_")
-                        $(def2).prop('id', def2_id)
-                        navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--sec" onclick="scrollToElement('${def2_id}')"> ${$(def2).text()}</div>`
-
-                        //set def3 lements ids, create submenu items
-                        $(def2).parent().find(`def3`).each((i, def3) => {
-                            let def3_id = ""+section+$(def3).text().replaceAll(" ", "_").replaceAll("%", "_").replaceAll("*", "star").replaceAll(".", "dot").replaceAll("(", "_").replaceAll(")", "_").replaceAll("/", "_")
-                            $(def3).prop('id', def3_id)
-                            navItemHtml += `<div class="nav_item_flex-container-item nav_item_flex-container-item--third" onclick="scrollToElement('${def3_id}')"> ${$(def3).text()}</div>`
-                        })
-                    })
-
-
-                })
-                //closing element of the submenu container
-
-                navItemHtml += `</div>`
-                //append submenu container with items to the navItem
-                $(navItem).append(navItemHtml)
-                hideLoader(250)
-            })
-        },250)
-    },250)
-})
-
-navItems.forEach(e => {
-    e.addEventListener("click", () => {
-
-        //get current window scroll
-        let currentScroll = window.pageYOffset
-
-        //store current scroll
-        secitonsPosition[currentSection] = currentScroll
-
-        //scroll top if new selection is the same as the current one
-        if(currentSection == e.getAttribute("section")){
-            // secitonsPosition[currentSection] = 0
-            // $("html, body").animate({ scrollTop: "0px" })
-        }
-
-        else {
-
-            $(`.nav_item-${currentSection}`).removeClass("nav_item--selected")
-            
-            //set new current selection
-            currentSection = e.getAttribute("section")
-            $(`.nav_item-${currentSection}`).addClass("nav_item--selected")
-
-            //set position when first time section selection
-            if(secitonsPosition[currentSection] == null) secitonsPosition[currentSection] = 0
-
-            //hide unselected sections
-            sections.forEach(s => { if(!s.classList.contains(`section-${currentSection}`)) $(s).hide() })
-
-            //show selected section
-            $(`.section-${currentSection}`).fadeIn(100)
-
-            //scrol to previously store scroll section after showing current section
-            $(window).scrollTop(secitonsPosition[currentSection])
-            document.title = "Unforgettable Gists - " + currentSection
-        }
-
-        setCookie('secitonsPosition', JSON.stringify(secitonsPosition), 1)
-        setCookie('currentSection', JSON.stringify(currentSection), 1)
-    })
-})
-
 
 function setCookie(key, value, expiry) {
     var expires = new Date()
@@ -578,7 +583,7 @@ var maxArea = function(height) {
 
 fetch('https://pokeapi.co/api/v2/pokemon/2').then( async r=> {
     r.json().then( e => {
-        console.log(e)
+        // console.log(e)
     })
 })
 
@@ -587,6 +592,6 @@ fetch('https://pokeapi.co/api/v2/pokemon/2').then( async r=> {
 
 fetch('https://pokeapi.co/api/v2/pokemon/2').then( async r=> {
     r.json().then( e => {        
-        console.log(e)
+        // console.log(e)
     })
 })
